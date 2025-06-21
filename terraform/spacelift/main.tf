a741936016bb20eaa5eea8270ede317f42d04417
John Flynn <JJimmyFlynn@gmail.com>
2025-06-20 23:50:53 -0400

    remove admin ability from ecs stack and set to auto deploy

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 resource "spacelift_stack" "prod-ecs-multi-region" {
-  administrative = true
   branch     = "main"
   name       = "prod-ecs-multi-region"
   repository = "ecs-multi-region-failover"
   project_root = "terraform/environments/prod"
   terraform_workflow_tool = "OPEN_TOFU"
+  autodeploy = true
 }
