3f3d3519a00d7ef5bbcbff680682b281713db4d9
John Flynn <JJimmyFlynn@gmail.com>
2025-06-20 23:49:29 -0400

    Minor fix for last commit

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -10,7 +10,7 @@ terraform {
 resource "spacelift_stack" "prod-ecs-multi-region" {
   administrative = true
   branch     = "main"
-  name       = "spacelift-self-management"
+  name       = "prod-ecs-multi-region"
   repository = "ecs-multi-region-failover"
   project_root = "terraform/environments/prod"
   terraform_workflow_tool = "OPEN_TOFU"
