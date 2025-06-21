a4fac544b4d5b4ddb9ca694873b8afdc010126a5
John Flynn <JJimmyFlynn@gmail.com>
2025-06-20 23:32:05 -0400

    Use terraform as root for spacelift

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -17,9 +17,8 @@ resource "spacelift_stack" "prod-ecs-multi-region-failover" {
   branch     = "main"
   name       = "prod-ecs-multi-region-failover"
   repository = "ecs-multi-region-failover"
-  project_root = "terraform/enviornments/prod"
+  project_root = "terraform"
   terraform_workflow_tool = "OPEN_TOFU"
-  additional_project_globs = ["terraform/spacelift"]
 }
 
 resource "spacelift_stack" "test" {
