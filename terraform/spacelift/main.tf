cbba42be0fc6fffbae3c67a8c2b0492836db4ee0
John Flynn <JJimmyFlynn@gmail.com>
2025-06-20 23:48:24 -0400

    Add ecs stack

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 
-resource "spacelift_stack" "spacelift" {
+resource "spacelift_stack" "prod-ecs-multi-region" {
   administrative = true
   branch     = "main"
   name       = "spacelift-self-management"
   repository = "ecs-multi-region-failover"
-  project_root = "terraform/spacelift"
+  project_root = "terraform/environments/prod"
   terraform_workflow_tool = "OPEN_TOFU"
 }
