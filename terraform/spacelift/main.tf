4e99407409acc919486ea06c3b44eddf84710c74
John Flynn <JJimmyFlynn@gmail.com>
2025-06-20 23:46:01 -0400

    Delete test stack

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -15,11 +15,3 @@ resource "spacelift_stack" "spacelift" {
   project_root = "terraform/spacelift"
   terraform_workflow_tool = "OPEN_TOFU"
 }
-
-resource "spacelift_stack" "test" {
-  branch     = "main"
-  name       = "test"
-  repository = "ecs-multi-region-failover"
-  project_root = "terraform/something"
-  terraform_workflow_tool = "OPEN_TOFU"
-}
