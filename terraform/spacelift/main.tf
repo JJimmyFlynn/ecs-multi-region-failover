fb25bf0b1179d57184175ef9982076cade4195b0
John Flynn <JJimmyFlynn@gmail.com>
2025-06-21 02:20:01 -0400

    Refactor stack to separate file

--- terraform/spacelift/main.tf
+++ terraform/spacelift/main.tf
@@ -6,24 +6,3 @@ terraform {
     }
   }
 }
-
-resource "spacelift_stack" "prod-ecs-multi-region" {
-  branch     = "main"
-  name       = "prod-ecs-multi-region"
-  repository = "ecs-multi-region-failover"
-  project_root = "terraform/environments/prod"
-  terraform_workflow_tool = "OPEN_TOFU"
-  autodeploy = true
-}
-
-resource "spacelift_aws_integration" "this" {
-  name     = "Infra POC 2025"
-  role_arn = "arn:aws:iam::383395885309:role/TerraformAccess"
-}
-
-resource "spacelift_aws_integration_attachment" "prod-ecs-multi-region" {
-  integration_id = spacelift_aws_integration.this.id
-  stack_id = spacelift_stack.prod-ecs-multi-region.id
-  read = true
-  write = true
-}
